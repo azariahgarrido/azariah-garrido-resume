e0cbe667d334ae8cd22a7f13e1d551f410bee049
azariahgarrido <azariahdanielgarrido@gmail.com>
2022-09-27 14:44:55 -0700

    Added mobile version

--- portfolio-resume/src/components/Footer.js
+++ portfolio-resume/src/components/Footer.js
@@ -1,10 +1,13 @@
 
 export default function foot() {
     return (
-        <div className="theToes">
-            <div><a href="https://github.com/azariahgarrido" target="_blank" rel="noreferrer"><img className="toe" src='https://www.pine64.org/wp-content/uploads/2019/04/github-logo.jpg' alt="FailedToLoadImg"></img></a></div>
-            <div><a href="https://www.linkedin.com/in/azariah-garrido-241792235/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.bBQANbh1YB53wIhXO2313gAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
-            <div><a href="https://www.facebook.com/azariah.garrido/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.xHe4-6tTi8FeIRYlG43PAgAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
+        <div className="desktopSwitch">
+            <div className="theToes">
+                <div><a href="https://github.com/azariahgarrido" target="_blank" rel="noreferrer"><img className="toe" src='https://www.pine64.org/wp-content/uploads/2019/04/github-logo.jpg' alt="FailedToLoadImg"></img></a></div>
+                <div><a href="https://www.linkedin.com/in/azariah-garrido-241792235/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.bBQANbh1YB53wIhXO2313gAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
+                <div><a href="https://www.facebook.com/azariah.garrido/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.xHe4-6tTi8FeIRYlG43PAgAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
+            </div>
+            <button className="mobileB"><a href="https://azariahgarrido.github.io/mobileFriendly/" target="_blank" rel="noreferrer">Switch to Mobile</a></button>
         </div>
     );
 }
